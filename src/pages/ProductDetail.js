cf28ec63712a30a62c3379f0fbdef6d698c89a2a
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-13 23:52:35 +0530

    used the useParam() hook in ProductDetail.js

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 const ProductDetail = () => {
+  const params = useParams();
   return (
     <section>
       <h1>ProductDetail</h1>
+      <p>{params.productId}</p>
     </section>
   );
 };
