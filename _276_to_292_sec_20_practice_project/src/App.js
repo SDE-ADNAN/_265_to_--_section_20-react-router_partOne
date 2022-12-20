e8bb6a08d336b5ec92b6d1249496d1eea997bf7c
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-20 23:58:27 +0530

    282 : Adding a "Not Found" Page

--- _276_to_292_sec_20_practice_project/src/App.js
+++ _276_to_292_sec_20_practice_project/src/App.js
@@ -2,6 +2,7 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import AllQuotes from "./Pages/AllQuotes";
 import NewQuote from "./Pages/NewQuote";
+import NotFound from "./Pages/NotFound";
 import QuoteDetail from "./Pages/QuoteDetail";
 
 function App() {
@@ -19,6 +20,9 @@ function App() {
         </Route>
         <Route path="/new-quote">
           <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
         </Route>
       </Switch>
     </Layout>
