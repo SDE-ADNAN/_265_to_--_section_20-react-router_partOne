3a955f84684337bae510dd0a19675f002a106316
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-20 07:53:53 +0530

    added dummy data

--- _276_to_292_sec_20_practice_project/src/Pages/AllQuotes.js
+++ _276_to_292_sec_20_practice_project/src/Pages/AllQuotes.js
@@ -2,7 +2,7 @@ import React from "react";
 import QuoteList from "../components/quotes/QuoteList";
 
 const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q1", author: "Max", text: "Learning React is fun!!" },
   { id: "q2", author: "Maximilian", text: "Learning React is great!" },
 ];
 
