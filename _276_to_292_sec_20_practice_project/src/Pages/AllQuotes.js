bc2d229d0f4aa14a6c0ea28287c13ca19ba02f14
SDE-ADNAN <adnansdeofficial@gmail.com>
2022-12-24 19:04:14 +0530

    added the Link component to allow to go to new-quote page if there are no quotes to display

--- _276_to_292_sec_20_practice_project/src/Pages/AllQuotes.js
+++ _276_to_292_sec_20_practice_project/src/Pages/AllQuotes.js
@@ -4,11 +4,6 @@ import QuoteList from "../components/quotes/QuoteList";
 import useHttp from "../hooks/use-http";
 import { getAllQuotes } from "../lib/api";
 
-// const DUMMY_QUOTES = [
-//   { id: "q1", author: "Max", text: "Learning React is fun!!" },
-//   { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-// ];
-
 const AllQuotes = () => {
   const {
     sendRequest,
